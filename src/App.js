de25a4ed8e3b65412bc6add86423b320ecdb6a69
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-02-19 22:29:02 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,50 +1,78 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header'
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo'
 
 import './App.css';
 
 class App extends Component {
-  state ={
+  state = {
     todos: [
       {
-        id:1,
+        id: 1,
         title: 'Take out the trash',
         completed: false
       },
       {
-        id:2,
+        id: 2,
         title: 'Dinner with katherine',
-        completed: true
+        completed: false
       },
       {
-        id:3,
+        id: 3,
         title: 'Meeting with boss',
         completed: false
       },
     ]
   }
-  
+
   //Toggle Complete
   toggleComplete = (id) => {
-    this.setState({todos: this.state.todos.map(todo => {  // // 
-      if(todo.id === id){
-        todo.completed = !todo.completed
-      }
-      return todo;
-    })});
+    this.setState({
+      todos: this.state.todos.map(todo => {  //  when the button is clicked, because the id is bound to the delTodo in the TodoItem it takes 
+        if (todo.id === id) {                                 // in the id then argument of the function. in the function is the filter method.using this in
+          todo.completed = !todo.completed                  // method we return any vale that is not (!==) equal to that id that has been plugged 
+        }
+        return todo;
+      })
+    });
   }
 
   //Delete To do
-  delTodo= (id) => {
-    console.log(id)
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
   }
-  
+
+  //Add Todo
+  addTodo = (title) => {
+    var last =  function(array, n) {
+      if (array == null) 
+        return void 0;
+      if (n == null) 
+         return array[array.length - 1];
+      return array.slice(Math.max(array.length - n, 0));  
+      };
+    
+    const newTodo={
+      id: (last(this.state.todos).id)+1,
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
-  
+
     return (
       <div className="App">
-        {/* line bellow passes the todos that are in the app component state to the todos component as a property. this information can now be accessed in the Todos.js*/}
-        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo}/> 
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          {/* line bellow passes the todos that are in the app component state to the todos component as a property. this information can now be accessed in the Todos.js*/}
+          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo} />
+
+        </div>
+
       </div>
     );
   }
